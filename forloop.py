a58c590236e1cf12446c921efd2058359d9eacfb
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-07-18 23:35:00 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,19 @@
+l = [10,20,30,40,50]
+
+
+
+for value in l:
+    print(value)
+
+
+sum = 100
+ 
+for value in l:
+	sum = sum +value
+	print(sum)
+
+
+	
+
+for value in range(1001,1021):
+	print(value)
